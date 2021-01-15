c8afcf4813a77ee2d2d95372afba216c40587d36
jucaran <jucaran@github.com>
2021-01-15 00:06:03 -0300

    changes sidebar funcionality

--- sidebar.js
+++ sidebar.js
@@ -1,13 +1,16 @@
 const sidebar = document.querySelector(".sidebar");
+const sidebarCloser = document.querySelector(".sidebar_closer");
 
 let menuOpen = false;
 
 const toggleMenu = () => {
   if (!menuOpen) {
     sidebar.classList.add("active");
+    sidebarCloser.classList.add("active");
     menuOpen = !menuOpen;
   } else {
     sidebar.classList.remove("active");
+    sidebarCloser.classList.add("active");
     menuOpen = !menuOpen;
   }
 };
