d762d5dc479d0157593e825b8aeaae60b50ec236
jucaran <jucaran@github.com>
2021-01-17 04:47:43 -0300

    adds a loader when the mail is being sended

--- sendMail.js
+++ sendMail.js
@@ -1,10 +1,14 @@
+const body = document.querySelector("body");
 const form = document.querySelector("form");
 const inputs = document.querySelectorAll(".form_input");
 const result = document.querySelector(".api_response");
 
+let mailSending = false;
+
 const sendMail = async (e) => {
   e.preventDefault();
   let formData = {};
+  body.classList.remove("loaded");
 
   inputs.forEach((e) => {
     formData[e.name] = e.value;
@@ -18,15 +22,12 @@ const sendMail = async (e) => {
     body: JSON.stringify(formData),
   });
 
-  // const final = await response.json();
-  // console.log("final", final);
-  console.log("response", response);
-  console.log("response status", response.ok);
-
   if (response.ok) {
+    body.classList.add("loaded");
     result.innerText = "Thank you, your message was delivered!";
     form.style.display = "none";
   } else {
+    body.classList.add("loaded");
     result.innerText = "Something went wrong :(";
     form.style.display = "none";
   }
