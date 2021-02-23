95cf0d6a74f8aa74697f5864c4101e9903e0ffdf
jucaran <jucaran@github.com>
2021-02-23 02:48:08 -0300

    finishes dark mode

--- scripts/sidebar.js
+++ scripts/sidebar.js
@@ -33,7 +33,24 @@ const toggleMode = () => {
   } else {
     document.querySelector("body").classList.remove("dark");
   }
+
+  if (window.location.pathname.split("/").pop() === "mywork.html") {
+    const gridItems = document.querySelectorAll(".work_item");
+    const [activeItem] = [...gridItems].filter((el) =>
+      el.classList.contains("active")
+    );
+    const [, activePosition] = activeItem && activeItem.classList;
+
+    if (activePosition) {
+      const [workLogoLight, workLogoDark] = document.querySelectorAll(
+        `.item_logo.${activePosition}`
+      );
+      workLogoLight.style.opacity = darkMode ? 0 : 1;
+      workLogoDark.style.opacity = darkMode ? 1 : 0;
+    }
+  }
+
   darkMode = !darkMode;
   localStorage.setItem("darkMode", darkMode);
-  toggleMenu();
+  menuOpen && toggleMenu();
 };
